2392e810928a05d5511327e78f2d98f23da8180f
Luís Felipe de Souza Silva <luis.felipeszsv@gmail.com>
2021-06-27 22:50:21 -0300

    creating error treatment

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import 'reflect-metadata';
 import './database';
 import { router } from './routes';
@@ -6,6 +7,18 @@ import { router } from './routes';
 const app = express();
 
 app.use(express.json());
+
 app.use(router);
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+    if(error instanceof Error) {
+        return response.status(400).json({ error: error.message});
+    }
+
+    return response.status(500).json({ 
+        status: 'error',
+        message: 'Internal Server Error'
+    });
+});
 
 app.listen(3000, () => console.log('server running on 3000'));
