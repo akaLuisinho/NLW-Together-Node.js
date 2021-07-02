51a58c719fdab599f11ed08653b90aa0595b5515
Luís Felipe de Souza Silva <luis.felipeszsv@gmail.com>
2021-07-02 00:57:08 -0300

    inserting user_id in request

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 
+interface IPayload {
+    sub: string;
+}
+
 export function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
 
     const authorization = request.headers.authorization;
@@ -12,8 +16,10 @@ export function ensureAuthenticated(request: Request, response: Response, next: 
     }
     
     try {
-        const authorized = verify(token, 'ae2774ab7bac678b3faa7381ce51238c');
+        const { sub } = verify(token, 'ae2774ab7bac678b3faa7381ce51238c') as IPayload;
 
+        request.user_id = sub;
+        
         return next();
     } catch (error) {
         return response.status(401).end;
