67d7ea333063cec10d303c9bd8fc55dd59a1b436
Luís Felipe de Souza Silva <luis.felipeszsv@gmail.com>
2021-07-03 16:46:43 -0300

    creating list user sent and received compliments methods

--- src/routes.ts
+++ src/routes.ts
@@ -5,9 +5,13 @@ import { CreateUserController } from './controllers/CreateUserController';
 import { CreateTagController } from './controllers/CreateTagController';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateComplimentController } from './controllers/CreateComplimentController';
+import { ListUserReceivedComplimentsController } from './controllers/ListUserReceivedComplimentsController';
+import { ListUserSentComplimentsController } from './controllers/ListUserSentComplimentsController';
 
 const router = Router();
 
+const listUserSentComplimentsController = new ListUserSentComplimentsController();
+const listUserReceivedComplimentsController = new ListUserReceivedComplimentsController();
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
@@ -17,5 +21,8 @@ router.post('/users', createUserController.handle);
 router.post('/login', authenticateUserController.handle);
 router.post('/tags', ensureAuthenticated, ensureAdmin, createTagController.handle);
 router.post('/compliments', ensureAuthenticated, createComplimentController.handle);
+
+router.get('/users/compliments/sent', ensureAuthenticated, listUserSentComplimentsController.handle);
+router.get('/users/compliments/received', ensureAuthenticated, listUserReceivedComplimentsController.handle);
 
 export { router };
